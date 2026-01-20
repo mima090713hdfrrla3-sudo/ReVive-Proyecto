e901b3abb4fb3431911b5e3a6979c42b498404cb
mima090713hdfrrla3-sudo <mima090713hdfrrla3.com>
2026-01-20 00:16:57 -0600

    Archivos desbloqueados y puerto configurado

--- app.py
+++ app.py
@@ -596,5 +596,7 @@ def delete_user(id):
     return redirect(url_for('admin_panel'))
 # --- MAIN ---
 if __name__ == '__main__':
-    init_db()
-    app.run()
+    import os
+    # Render asigna un puerto dinámico, esto lo detecta automáticamente
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
